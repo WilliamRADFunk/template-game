83100ce4dec6ad40433bdd6eedc9a07eb44dc241
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2020-08-25 10:49:03 -0700

    Tries using object3D groups for circular thrusters.

--- src/ts/scenes/ancient-ruins/utils/thruster-circular.ts
+++ src/ts/scenes/ancient-ruins/utils/thruster-circular.ts
@@ -2,20 +2,16 @@ import {
     Mesh,
     MeshBasicMaterial,
     Scene,
-    CircleGeometry} from 'three';
+    CircleGeometry,
+    Object3D} from 'three';
 
 import { SOUNDS_CTRL } from '../../../controls/controllers/sounds-controller';
 
 const POSITION_MODS: [number, number][] = [
-    [0, 0],
-    [0, 0],
-    [0, 0],
-    [0.05, 0.05],
-    [-0.05, -0.05],
-    [0.05, -0.05],
-    [-0.05, 0.05],
-    [-0.075, 0.05],
-    [-0.05, 0.075]
+    [-0.3, -0.3],
+    [0.3, -0.3],
+    [-0.3, 0.3],
+    [0.3, 0.3],
 ];
 
 const ORANGE: string = '#FF6800';
@@ -30,7 +26,7 @@ export class CircularThruster {
      * Controls the overall rendering of the various flames.
      */
 
-    private _flames: Mesh[] = [];
+    private _flames: Object3D[] = [];
 
     /**
      * Reference to the scene, used to and and remove flames from rendering cycle once finished.
@@ -80,70 +76,81 @@ export class CircularThruster {
      * @param position x, y, z coordinate for base of flames.
      */
     private _createFlames(position: [number, number, number]): void {
-        const geo = new CircleGeometry(0.75, 32);
+        const geo = new CircleGeometry(0.25, 32);
+
+        const yellowFlames = new Object3D();
+        const orangeFlames = new Object3D();
+        const redFlames = new Object3D();
 
         const meshYellow = new Mesh(geo, this._yellowFlameMaterial);
         meshYellow.position.set(position[0] + POSITION_MODS[0][0], position[1], position[2] + POSITION_MODS[0][1]);
         meshYellow.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshYellow);
-        this._scene.add(meshYellow);
-        meshYellow.visible = false;
-
-        const meshOrange = new Mesh(geo, this._orangeFlameMaterial);
-        meshOrange.position.set(position[0] + POSITION_MODS[1][0], position[1], position[2] + POSITION_MODS[1][1]);
-        meshOrange.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshOrange);
-        this._scene.add(meshOrange);
-        meshOrange.visible = false;
-
-        const meshRed = new Mesh(geo, this._redFlameMaterial);
-        meshRed.position.set(position[0] + POSITION_MODS[2][0], position[1], position[2] + POSITION_MODS[2][1]);
-        meshRed.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshRed);
-        this._scene.add(meshRed);
-        meshRed.visible = false;
+        yellowFlames.add(meshYellow);
 
         const meshYellow2 = new Mesh(geo, this._yellowFlameMaterial);
-        meshYellow2.position.set(position[0] + POSITION_MODS[3][0], position[1], position[2] + POSITION_MODS[3][1]);
+        meshYellow2.position.set(position[0] + POSITION_MODS[1][0], position[1], position[2] + POSITION_MODS[1][1]);
         meshYellow2.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshYellow2);
-        this._scene.add(meshYellow2);
-        meshYellow2.visible = false;
-
-        const meshOrange2 = new Mesh(geo, this._orangeFlameMaterial);
-        meshOrange2.position.set(position[0] + POSITION_MODS[4][0], position[1], position[2] + POSITION_MODS[4][1]);
-        meshOrange2.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshOrange2);
-        this._scene.add(meshOrange2);
-        meshOrange2.visible = false;
-
-        const meshRed2 = new Mesh(geo, this._redFlameMaterial);
-        meshRed2.position.set(position[0] + POSITION_MODS[5][0], position[1], position[2] + POSITION_MODS[5][1]);
-        meshRed2.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshRed2);
-        this._scene.add(meshRed2);
-        meshRed2.visible = false;
+        yellowFlames.add(meshYellow2);
 
         const meshYellow3 = new Mesh(geo, this._yellowFlameMaterial);
-        meshYellow3.position.set(position[0] + POSITION_MODS[6][0], position[1], position[2] + POSITION_MODS[6][1]);
+        meshYellow3.position.set(position[0] + POSITION_MODS[2][0], position[1], position[2] + POSITION_MODS[2][1]);
         meshYellow3.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshYellow3);
-        this._scene.add(meshYellow3);
-        meshYellow3.visible = false;
+        yellowFlames.add(meshYellow3);
 
-        const meshOrange3 = new Mesh(geo, this._orangeFlameMaterial);
-        meshOrange3.position.set(position[0] + POSITION_MODS[7][0], position[1], position[2] + POSITION_MODS[7][1]);
+        const meshYellow4 = new Mesh(geo, this._yellowFlameMaterial);
+        meshYellow4.position.set(position[0] + POSITION_MODS[3][0], position[1], position[2] + POSITION_MODS[3][1]);
+        meshYellow4.rotation.set(-1.5708, 0, 0);
+        yellowFlames.add(meshYellow4);
+
+        const meshOrange = new Mesh(geo, this._yellowFlameMaterial);
+        meshOrange.position.set(position[0] + POSITION_MODS[0][0], position[1], position[2] + POSITION_MODS[0][1]);
+        meshOrange.rotation.set(-1.5708, 0, 0);
+        orangeFlames.add(meshOrange);
+
+        const meshOrange2 = new Mesh(geo, this._yellowFlameMaterial);
+        meshOrange2.position.set(position[0] + POSITION_MODS[1][0], position[1], position[2] + POSITION_MODS[1][1]);
+        meshOrange2.rotation.set(-1.5708, 0, 0);
+        orangeFlames.add(meshOrange2);
+
+        const meshOrange3 = new Mesh(geo, this._yellowFlameMaterial);
+        meshOrange3.position.set(position[0] + POSITION_MODS[2][0], position[1], position[2] + POSITION_MODS[2][1]);
         meshOrange3.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshOrange3);
-        this._scene.add(meshOrange3);
-        meshOrange3.visible = false;
+        orangeFlames.add(meshOrange3);
 
-        const meshRed3 = new Mesh(geo, this._redFlameMaterial);
-        meshRed3.position.set(position[0] + POSITION_MODS[8][0], position[1], position[2] + POSITION_MODS[8][1]);
+        const meshOrange4 = new Mesh(geo, this._yellowFlameMaterial);
+        meshOrange4.position.set(position[0] + POSITION_MODS[3][0], position[1], position[2] + POSITION_MODS[3][1]);
+        meshOrange4.rotation.set(-1.5708, 0, 0);
+        orangeFlames.add(meshOrange4);
+
+        const meshRed = new Mesh(geo, this._yellowFlameMaterial);
+        meshRed.position.set(position[0] + POSITION_MODS[0][0], position[1], position[2] + POSITION_MODS[0][1]);
+        meshRed.rotation.set(-1.5708, 0, 0);
+        redFlames.add(meshRed);
+
+        const meshRed2 = new Mesh(geo, this._yellowFlameMaterial);
+        meshRed2.position.set(position[0] + POSITION_MODS[1][0], position[1], position[2] + POSITION_MODS[1][1]);
+        meshRed2.rotation.set(-1.5708, 0, 0);
+        redFlames.add(meshRed2);
+
+        const meshRed3 = new Mesh(geo, this._yellowFlameMaterial);
+        meshRed3.position.set(position[0] + POSITION_MODS[2][0], position[1], position[2] + POSITION_MODS[2][1]);
         meshRed3.rotation.set(-1.5708, 0, 0);
-        this._flames.push(meshRed3);
-        this._scene.add(meshRed3);
-        meshRed3.visible = false;
+        redFlames.add(meshRed3);
+
+        const meshRed4 = new Mesh(geo, this._yellowFlameMaterial);
+        meshRed4.position.set(position[0] + POSITION_MODS[3][0], position[1], position[2] + POSITION_MODS[3][1]);
+        meshRed4.rotation.set(-1.5708, 0, 0);
+        redFlames.add(meshRed4);
+
+        this._flames.push(yellowFlames);
+        this._flames.push(orangeFlames);
+        this._flames.push(redFlames);
+        yellowFlames.visible = false;
+        orangeFlames.visible = false;
+        redFlames.visible = false;
+        this._scene.add(yellowFlames);
+        this._scene.add(orangeFlames);
+        this._scene.add(redFlames);
     }
 
     public dispose(): void {
@@ -167,15 +174,21 @@ export class CircularThruster {
                     SOUNDS_CTRL.playMainThrusterSmall();
                 });
             }
-            this._flames.forEach(flame => {
-                const currScale = flame.scale;
-                flame.scale.set(currScale.x + scaleMod, currScale.y + scaleMod, currScale.z + scaleMod);
-                const currOpacity = (flame.material as MeshBasicMaterial).opacity;
-                if (currOpacity <= 0.1) {
-                    (flame.material as MeshBasicMaterial).opacity = 0.8;
-                } else {
-                    (flame.material as MeshBasicMaterial).opacity = currOpacity - 0.05;
-                }
+            this._flames.forEach(flameGroup => {
+                flameGroup.children.forEach((flame: Mesh) => {
+                    if (flame.isMesh) {
+                        console.log('isMesh');
+                        const currScale = flame.scale;
+                        flame.scale.set(currScale.x + scaleMod, currScale.y + scaleMod, currScale.z + scaleMod);
+                        const currOpacity = (flame.material as MeshBasicMaterial).opacity;
+                        if (currOpacity <= 0.1) {
+                            (flame.material as MeshBasicMaterial).opacity = 0.8;
+                        } else {
+                            (flame.material as MeshBasicMaterial).opacity = currOpacity - 0.05;
+                        }
+                        flame.updateMatrix();
+                    }
+                });
             });
         } else if (this._flames[0].visible) {
             this._flames.forEach(flame => {
@@ -185,7 +198,8 @@ export class CircularThruster {
             });
         }
         this._flames.forEach((flame, index) => {
-            flame.position.set(position[0] + POSITION_MODS[index][0], position[1], position[2] + POSITION_MODS[index][1]);
+            flame.position.set(0, position[1], 0);
+            flame.updateMatrix();
         });
     }
 }
