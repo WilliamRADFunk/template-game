7d935a8869bb02ce1583e8ceab63ab338187b3fd
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2020-09-22 09:28:07 -0700

    Better styling for the modal dialogue.

--- src/ts/controls/controllers/modal-dialogue-controller.ts
+++ src/ts/controls/controllers/modal-dialogue-controller.ts
@@ -30,11 +30,42 @@ export class ModalDialogueCtrl {
      * Constructor for the modal dialogue Base Controller class.
      */
     constructor (container: HTMLElement) {
-        this._backdrop = document.createElement('div');
-        this._box = document.createElement('div');
-        this._description = document.createElement('p');
-        this._description.style.fontFamily = 'Luckiest Guy';
+        // Create modal container.
         this._wrapper = document.createElement('div');
+        this._wrapper.style.left = '0px';
+        this._wrapper.style.top = '0px';
+        this._wrapper.style.position = 'absolute';
+        this._wrapper.style.zIndex = '10';
+
+        // Create opaque backdrop.
+        this._backdrop = document.createElement('div');
+        this._backdrop.style.left = '0px';
+        this._backdrop.style.top = '0px';
+        this._backdrop.style.opacity = '0.7';
+        this._backdrop.style.display = 'block';
+        this._backdrop.style.position = 'absolute';
+        this._backdrop.style.zIndex = '10';
+
+        // Create modal box.
+        this._box = document.createElement('div');
+        this._box.style.backgroundColor = '#89CFF0';
+        this._box.style.border = '5px solid #FEDC56';
+        this._box.style.borderRadius = '6px';
+        this._box.style.boxShadow = '#666666 2px 2px 2px';
+        this._box.style.display = 'block';
+        this._box.style.position = 'absolute';
+        this._box.style.zIndex = '20';
+        this._box.style.marginLeft = 'auto';
+        this._box.style.marginRight = 'auto';
+
+        // Create main text.
+        this._description = document.createElement('p');
+        this._description.style.color = '#DDDDDD';
+        this._description.style.fontFamily = 'Luckiest Guy';
+        this._description.style.lineHeight = '1.1';
+        this._description.style.textShadow = '0px 1px 1px #000000';
+
+        // Append.
         this._wrapper.appendChild(this._backdrop);
         this._wrapper.appendChild(this._box);
         this._box.appendChild(this._description);
@@ -59,30 +90,14 @@ export class ModalDialogueCtrl {
     public reposition(position?: HTMLElementPosition): void {
         this._wrapper.style.width = position.width + 'px';
         this._wrapper.style.height = position.height + 'px';
-        this._wrapper.style.left = '0px';
-        this._wrapper.style.top = '0px';
-        this._wrapper.style.position = 'absolute';
-        this._wrapper.style.zIndex = '10';
         this._backdrop.style.width = position.width + 'px';
         this._backdrop.style.height = position.height + 'px';
-        this._backdrop.style.left = '0px';
-        this._backdrop.style.top = '0px';
-        this._backdrop.style.opacity = '0.7';
-        this._backdrop.style.display = 'block';
-        this._backdrop.style.position = 'absolute';
-        this._backdrop.style.zIndex = '10';
         this._box.style.width = (position.width * 0.7) + 'px';
         this._box.style.height = (position.height * 0.35) + 'px';
         this._box.style.left = position.left + 'px';
         this._box.style.top = (position.height * 0.2)  + 'px';
-        this._box.style.backgroundColor = '#CCCCCC';
-        this._box.style.border = '3px solid white';
-        this._box.style.borderRadius = '4px';
-        this._box.style.display = 'block';
-        this._box.style.position = 'absolute';
-        this._box.style.zIndex = '20';
-        this._box.style.marginLeft = 'auto';
-        this._box.style.marginRight = 'auto';
+        this._description.style.fontSize = (position.width * 0.0172) + 'px';
+        this._description.style.padding = (position.width * 0.01) + 'px';
     }
 
     /**
